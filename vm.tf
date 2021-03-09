fa0b30378a41b690e375e741643b50765d790b2d
amin nik <newaminazur@outlook.com>
2021-03-08 20:22:01 -0500

    fixed windows os profile

--- vm.tf
+++ vm.tf
@@ -1,36 +1,24 @@
-# Create a Core server virtual machine
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "server01"
-  location              = var.location3
-  resource_group_name   = azurerm_resource_group.ab.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
 
-  storage_os_disk {
-    name              = "server_os"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "windows vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_image_reference {
-    publisher = "Acronis"
-    offer     = "windowsServer"
-    sku       = "2019-Datacenter-Core-smalldisk"
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
-
-  os_profile {
-    computer_name  = "winS2019"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-    os_profile_windows_config {
-  
-}
-}
-data "azurerm_public_ip" "ip" {
-  name                = azurerm_public_ip.publicip.name
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on          = [azurerm_virtual_machine.vm]
 }
