90d2769216972c4574644c0bc926fdd3306e2c06
amin nik <newaminazur@outlook.com>
2021-03-08 21:32:38 -0500

    all good

--- vm.tf
+++ vm.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "windows vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
+resource "azurerm_windows_virtual_machine" "nic" {
+  name                = "windows-vm"
+  resource_group_name = azurerm_resource_group.ab.name
+  location            = var.location3
+    size                = "Standard_F2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.nic.id
   ]
 
   os_disk {
